e65d7565b49b9d04c0bea43d130f14253b14edbb
Gadi Cohen <dragon@wastelands.net>
2020-12-13 11:12:49 +0000

    add json5 loader

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     dest: 'public'
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+
+    config.module.rules.push({
+      test: /\.json5$/i,
+      loader: 'json5-loader',
+      type: 'javascript/auto'
+    });
+
+    return config
+  },
 });
