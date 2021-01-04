0957dc4edc5c1ce16bd8cf268c4bb11d0e1d8a02
Gadi Cohen <dragon@wastelands.net>
2021-01-04 10:27:27 +0200

    _doc: add fb:admins meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,7 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
           <meta name="theme-color" content="#ffffff" />
+          <meta property="fb:admins" content="gadicohen"/>
         </Head>
         <body>
           <Main />
