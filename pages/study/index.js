7d65d54257a637bcbd4126335a1e72c1ca542c8b
Gadi Cohen <dragon@wastelands.net>
2022-03-28 13:57:24 +0100

    Study: notes on requests and private material

--- pages/study/index.js
+++ pages/study/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
+import Typography from "@mui/material/Typography";
 
 import Link from "../../src/Link.js";
 import AppBar from "../../components/AppBar.js";
@@ -13,13 +14,26 @@ export default function Study() {
     <>
       <AppBar title="Study" />
       <Container>
-        <h1>Current Sets</h1>
-        <h1>All Sets</h1>
-        {Object.keys(sets).map((name) => (
-          <React.Fragment key={name}>
-            <Chip component={Link} href={"/study/" + name} label={name} />{" "}
-          </React.Fragment>
-        ))}
+        <Typography variant="h5">Current Sets</Typography>
+        <p>Coming one day.</p>
+        <Typography variant="h5">All Sets</Typography>
+        <div>
+          {Object.keys(sets).map((name) => (
+            <React.Fragment key={name}>
+              <Chip component={Link} href={"/study/" + name} label={name} />{" "}
+            </React.Fragment>
+          ))}
+        </div>
+        <br />
+        <Typography variant="h5">Requests</Typography>
+        <Typography variant="body2">
+          Happily taking requests for new sets, contact details on the{" "}
+          <Link href="/about">About</Link> page. NB: I can only add data from
+          public sources. Please don&apos;t send me any private Order documents.
+          However, the heads of your order are welcome to make contact to have
+          such data made available only to members of your order in a secure
+          way.
+        </Typography>
       </Container>
     </>
   );
