b6c70974dcd029ce8ea45a6dce21bbcebd986c3a
Gadi Cohen <dragon@wastelands.net>
2022-04-05 15:10:59 +0100

    wow, what a mish.  sidestep serverside esm module issues.

--- pages/doc/[_id].js
+++ pages/doc/[_id].js
@@ -1,14 +1,49 @@
 import React from "react";
-import { Render } from "json-rich-text";
+import dynamic from "next/dynamic";
+
+// v1.1.1 without cjs.  TODO i guess.
+import { Render, registerBlockType } from "json-rich-text";
+//import Node from "json-rich-text/lib/esm/blocks/node.js";
 
 import AppBar from "../../components/AppBar.js";
 import neophyte from "./neophyte.yaml";
-const doc = { children: neophyte };
 
-console.log({ doc });
+const origDoc = { children: neophyte };
 
-export default function Doc() {
+const win = typeof window === "object";
+if (win) {
+  (async function () {
+    const Node = (await import("json-rich-text/lib/esm/blocks/node.js"))
+      .default;
+    console.log({ Node });
+
+    const types = {
+      todo: class Todo2 extends Node {
+        //type: "todo";
+
+        render(key) {
+          return <div key={key}>{this.block?.title}</div>;
+        }
+      },
+
+      task: class Task extends Node {
+        //type: "todo";
+
+        render(key) {
+          return <div key={key}>{this.block?.title}</div>;
+        }
+      },
+    };
+
+    for (const [key, val] of Object.entries(types)) registerBlockType(key, val);
+  })();
+}
+
+function Doc() {
   //const doc = { children: [{ type: "text", value: "hi" }] };
+  const [doc, setDoc] = React.useState(origDoc);
+  window.setDoc = setDoc;
+  window.origDoc = origDoc;
 
   return (
     <>
@@ -17,3 +52,5 @@ export default function Doc() {
     </>
   );
 }
+
+export default dynamic(Promise.resolve(Doc), { ssr: false });
