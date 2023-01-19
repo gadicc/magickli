b3192b62e95b5fbb835ffc0414940f8124b8ce62
Gadi Cohen <dragon@wastelands.net>
2023-01-19 14:47:25 +0200

    fix(sets): import from ... "data" not "data.js"

--- src/study/sets.tsx
+++ src/study/sets.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
 
-import data from "../../data/data.js";
+import data from "../../data/data";
 
 function SingleCharQuestion({ question }: { question: string }) {
   return (
