2af1dd53ae101823019b1aeb9e09e651e62aa6ba
Gadi Cohen <dragon@wastelands.net>
2023-09-24 22:33:39 +0100

    feat(72angels): initial work

---
+++ data/kabbalah/SeventyTwoAngels.ts
@@ -0,0 +1,28 @@
+// import type { AngelicOrder } from "./AngelicOrders";
+
+// type LangObject = { he: string; roman: string };
+
+interface Angel {
+  name: { en: string; he: string };
+  text: { en: string; fr: string };
+  governs: {
+    startDate: {
+      day: number;
+      month: number;
+    };
+    endDate: {
+      day: number;
+      month: number;
+    };
+  };
+  godName: string;
+  angelicOrderId: string;
+  // angelicOrder: AngelicOrder;
+}
+
+type Angels = Angel[];
+
+const angels: Angels = require("./seventyTwoAngels.json5").default;
+
+export type { Angel, Angels };
+export default angels;
---
+++ pages/kabbalah/yhvh/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+//import Link from '@mui/material/Link';
+
+import AppBar from "../../../components/AppBar";
+import Tiles from "../../../components/Tiles";
+
+const tiles = [
+  {
+    Component: () => (
+      <div style={{ margin: "auto", textAlign: "center" }}>72 Angels</div>
+    ),
+    title: "72 Angels",
+    to: "/kabbalah/yhvh/72",
+  },
+];
+
+function Kabbalah() {
+  const navParts = [{ title: "Kabbalah", url: "/kabbalah" }];
+
+  return (
+    <>
+      <AppBar title="Shem Hamephorash" navParts={navParts} />
+      <Tiles tiles={tiles} />
+    </>
+  );
+}
+
+export default Kabbalah;
