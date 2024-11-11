40717142a7ef886fbe01b4d0c7139f9bf9f4d335
Gadi Cohen <dragon@wastelands.net>
2024-11-11 10:12:01 +0000

    fix(trpc): dev/prod urls

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -4,13 +4,17 @@ import type { AppRouter } from "@/app/api/trpc/appRouter";
 //     👆 **type-only** import
 import superjson from "superjson";
 
+const origin =
+  typeof window === "undefined"
+    ? "http://localhost:3000"
+    : window.location.origin;
+
 // Pass AppRouter as generic here. 👇 This lets the `trpc` object know
 // what procedures are available on the server and their input/output types.
 const trpc = createTRPCClient<AppRouter>({
   links: [
     unstable_httpBatchStreamLink({
-      // url: "http://localhost:3000",
-      url: window.location.origin + "/api/trpc",
+      url: origin + "/api/trpc",
       transformer: superjson,
     }),
   ],
