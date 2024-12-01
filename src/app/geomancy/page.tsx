2e802dd47e2a9d004d39568733ad96c54ede484c
Gadi Cohen <dragon@wastelands.net>
2024-12-01 10:53:50 +0000

    fix(geomancy): better tile images

--- src/app/geomancy/page.tsx
+++ src/app/geomancy/page.tsx
@@ -1,19 +1,36 @@
 import React from "react";
+import Image from "next/image";
 //import Link from '@mui/material/Link';
 
 import Tiles from "@/components/Tiles";
-import GeomanticFigures from "./Geomantic_figures.svg";
+
+import ReadingImgTile from "./img/reading.webp";
+import ReferenceImgTile from "./img/reference.webp";
 
 const tiles = [
   {
-    Component: () => <GeomanticFigures width="100%" />,
-    title: "Reference",
-    to: "/geomancy/reference",
-  },
-  {
-    Component: () => <GeomanticFigures width="100%" />,
     title: "Reading",
     to: "/geomancy/reading",
+    Component: () => (
+      <Image
+        src={ReadingImgTile}
+        style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        alt="Geomancy Reading"
+        sizes="(max-width: 1200px) 300px"
+      />
+    ),
+  },
+  {
+    title: "Reference",
+    to: "/geomancy/reference",
+    Component: () => (
+      <Image
+        src={ReferenceImgTile}
+        style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        alt="Geomancy Reference"
+        sizes="(max-width: 1200px) 300px"
+      />
+    ),
   },
 ];
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Image from "next/image";
-//import Link from '@mui/material/Link';
-
-import Tiles from "@/components/Tiles";
-
-import GDLogoSquished from "@/goldendawn-logo-squished.svg";
-import GeomanticFigures from "@/app/geomancy/Geomantic_figures.svg";
-import MagicalTemple from "./img/magical-temple.webp";
 import { useGongoOne, useGongoUserId } from "gongo-client-react";
+
 import {
   AdminPanelSettings,
   AdminPanelSettingsTwoTone,
 } from "@mui/icons-material";
+//import Link from '@mui/material/Link';
+
+import Tiles from "@/components/Tiles";
+import GDLogoSquished from "@/goldendawn-logo-squished.svg";
+import GeomanticFigures from "@/app/geomancy/Geomantic_figures.svg";
+import MagicalTemple from "./img/magical-temple.webp";
+import GeomancyImg from "@/app/img/geomancy.webp";
 
 const tiles = [
   {
@@ -30,9 +31,16 @@ const tiles = [
     to: "/enochian/",
   },
   {
-    Component: () => <GeomanticFigures width="100%" />,
     title: "Geomancy",
     to: "/geomancy/",
+    Component: () => (
+      <Image
+        src={GeomancyImg}
+        style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        alt="A shaman pokes holes in the sand"
+        sizes="(max-width: 1200px) 300px"
+      />
+    ),
   },
   {
     Component: () => <GDLogoSquished width="100%" />,
