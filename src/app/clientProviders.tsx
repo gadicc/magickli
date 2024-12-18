4f71f7c4ab5e3f4127a9cd630db66e5e5d461163
Gadi Cohen <dragon@wastelands.net>
2024-12-18 13:06:32 +0200

    fix(layout): add workboxStuff to clientProviders

--- src/app/clientProviders.tsx
+++ src/app/clientProviders.tsx
@@ -1,13 +1,19 @@
 "use client";
+import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import workboxStuff from "@/workboxStuff";
 
 export default function ClientProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  React.useEffect(() => {
+    workboxStuff();
+  }, []);
+
   return (
     <SessionProvider>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
